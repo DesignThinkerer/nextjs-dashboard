8588a7ab4392b67a6c2a1ba199ffaa2b3b0da64e
Théophile <34781168+DesignThinkerer@users.noreply.github.com>
2024-07-09 00:27:29 +0200

    delete invoice button

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -12,6 +12,9 @@ const FormSchema = z.object({
     status: z.enum(['pending', 'paid']),
     date: z.string(),
 });
+
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
  
 
 const CreateInvoice = FormSchema.omit({id: true, date: true});
@@ -33,5 +36,29 @@ export async function createInvoice(formData: FormData) {
     //update cached data
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
+  }
+  
+  export async function updateInvoice(id: string, formData: FormData) {
+    const { customerId, amount, status } = UpdateInvoice.parse({
+      customerId: formData.get('customerId'),
+      amount: formData.get('amount'),
+      status: formData.get('status'),
+    });
+   
+    const amountInCents = amount * 100;
+   
+    await sql`
+      UPDATE invoices
+      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+      WHERE id = ${id}
+    `;
+   
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
+  }
+  
+  export async function deleteInvoice(id: string) {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+    revalidatePath('/dashboard/invoices');
   }
   
